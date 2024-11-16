f38f93a620d27b1eee9136688e732643a60eb974
Dixit Akabari <ba.akabaridixit@tataelxsi.co.in>
2024-11-16 23:50:52 +0530

    init project.

---
+++ News/News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Akabari Dixit on 16/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
